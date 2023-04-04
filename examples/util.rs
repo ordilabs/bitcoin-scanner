8e8809424749e40b07ee7eaf5d25c6ed0f64a622
Fabian Jahr <fjahr@protonmail.com>
2023-04-04 12:29:06 +0200

    Make examples robust for pruning locally and alternative datadir path

--- examples/util.rs
+++ examples/util.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 extern crate directories;
 use directories::BaseDirs;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 pub fn bitcoin_data_dir(network: bitcoin::Network) -> PathBuf {
     let mut data_dir: PathBuf = BaseDirs::new().unwrap().data_dir().into();
@@ -27,10 +29,37 @@ pub fn bitcoin_data_dir(network: bitcoin::Network) -> PathBuf {
         }
     };
 
-    data_dir
+    // Check if bitcoin.conf contains a datadir setting and if so, use that instead.
+    let bitcoin_conf_path = data_dir.join("bitcoin.conf");
+
+    if bitcoin_conf_path.exists() {
+        let datadir = find_datadir(bitcoin_conf_path).unwrap();
+        if datadir.is_some() {
+            return datadir.unwrap();
+        }
+    }
+
+    return data_dir;
 }
 
 #[allow(dead_code)]
 pub fn main() {
     print!("Hello, world!");
+}
+
+fn find_datadir(bitcoin_conf_path: PathBuf) -> io::Result<Option<PathBuf>> {
+    let file = File::open(bitcoin_conf_path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        let trimmed_line = line.trim();
+
+        if trimmed_line.starts_with("datadir=") {
+            let datadir = trimmed_line.strip_prefix("datadir=").unwrap();
+            return Ok(Some(PathBuf::from(datadir)));
+        }
+    }
+
+    Ok(None)
 }
